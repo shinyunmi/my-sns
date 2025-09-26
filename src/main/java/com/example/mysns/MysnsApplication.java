45fee2805f226425b3b7819d5d87073245160380
Yunmi <tlsdbs45@naver.com>
2025-09-26 10:36:13 +0900

    초기 프로젝트 셋업

---
+++ src/main/java/com/example/mysns/MysnsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysnsApplication.class, args);
+	}
+
+}
